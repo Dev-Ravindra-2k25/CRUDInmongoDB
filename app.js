9aa6c37ec88a34cfe127e20474acc6ac0a470751
Ravindra G <rg4079099@gmail.com>
2026-06-29 23:31:46 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const path = require("path");
+const User = require("./models/user");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home Page (Display Users)
+app.get("/", async (req, res) => {
+    const users = await User.find();
+    res.render("index", { users });
+});
+
+// Create User Page
+app.get("/create", (req, res) => {
+    res.render("create");
+});
+
+// Create User
+app.post("/create", async (req, res) => {
+    const { name, username, email } = req.body;
+
+    await User.create({
+        name,
+        username,
+        email
+    });
+
+    res.redirect("/");
+});
+
+// Open Edit Page
+app.get("/edit/:id", async (req, res) => {
+
+    const user = await User.findById(req.params.id);
+
+    res.render("edit", { user });
+
+});
+
+// Update User
+app.post("/update/:id", async (req, res) => {
+
+    const { name, username, email } = req.body;
+
+    await User.findByIdAndUpdate(req.params.id, {
+        name,
+        username,
+        email
+    });
+
+    res.redirect("/");
+
+});
+
+app.get("/delete/:id", async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.redirect("/");
+});
+
+
+app.listen(3000, () => {
+    console.log("Server Started and working fine");
+});
---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/monster");
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    username: String,
+    email: String,
+    contact:String,
+});
+//                      User is model..without that 
+//no operation like find,delete...can be performed  
+
+module.exports = mongoose.model("User", userSchema);
